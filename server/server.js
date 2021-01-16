9736dab85e201a958f0dedd3ea290c085d5cbf4e
Felipe <fepipe82@hotmail.com>
2021-01-16 18:05:57 -0300

    Commit Inicial

---
+++ server/server.js
@@ -0,0 +1,54 @@
+require('./config/config');
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(express.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+//app.use(express.json());
+
+app.get('/usuario', function(req, res) {
+    res.json('get Usuario');
+});
+
+// crear registros
+app.post('/usuario', function(req, res) {
+
+    let body = req.body;
+
+    if (body.nombre === undefined) {
+
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        });
+
+    } else {
+        res.json({
+            persona: body
+        });
+    }
+});
+
+//actualizar registros
+app.put('/usuario/:id', function(req, res) {
+
+    let id = req.params.id;
+
+    res.json({
+        id
+    });
+});
+
+app.delete('/usuario', function(req, res) {
+    res.json('delete Usuario');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando puerto', 3000);
+});
